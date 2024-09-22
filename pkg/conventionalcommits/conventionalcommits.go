9c1cda61976156f6341f83845167ec5abaf09fed
John Woffindin <john.woffindin@cloverhealth.com>
2024-09-23 08:16:00 +1200

    Add Issues to commit structure

--- pkg/conventionalcommits/conventionalcommits.go
+++ pkg/conventionalcommits/conventionalcommits.go
@@ -5,15 +5,14 @@ import (
 	"fmt"
 )
 
-var (
-	Marshal = json.Marshal
-)
+var Marshal = json.Marshal
 
 // ConventionalCommits a slice of parsed conventional commit messages
 type ConventionalCommits []*ConventionalCommit
 
 // ConventionalCommit a parsed conventional commit message
 type ConventionalCommit struct {
+	Issues      []string `json:"issues"`
 	Category    string   `json:"category"`
 	Scope       string   `json:"scope"`
 	Description string   `json:"description"`
